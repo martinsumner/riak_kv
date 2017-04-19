92faa916fd626eb175db106d83a7e16d22b57d8a
Martin Sumner <martin.sumner@adaptip.co.uk>
2017-04-19 22:48:39 +0100

    Restrict compaction
    Temporary measure to aid testing

--- include/riak_kv_leveled.hrl
+++ include/riak_kv_leveled.hrl
@@ -6,7 +6,7 @@
 -define(LEVELED_SYNCSTRATEGY, none). % can be riak_sync|none
 -define(LEVELED_JOURNALSIZE, 500000000).
 -define(LEVELED_LEDGERCACHE, 4000).
--define(LEVELED_JC_VALID_HOURS, [4, 5, 6]).
+-define(LEVELED_JC_VALID_HOURS, [7, 8]).
 -define(LEVELED_JC_CHECK_INTERVAL, timer:minutes(10)).
 -define(LEVELED_JC_CHECK_JITTER, 0.3).
 -define(LEVELED_DATAROOT, "/data/leveled").
