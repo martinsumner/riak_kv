5ed22c4d063e9915c0f7ab5c9c4454d436d41903
Martin Sumner <martin.sumner@adaptip.co.uk>
2025-03-21 16:27:55 +0000

    Don't stop if already undefined
    Makes leveled consistent with other backends

--- src/riak_kv_leveled_backend.erl
+++ src/riak_kv_leveled_backend.erl
@@ -78,7 +78,7 @@
     % accepting HEAD/GET/FOLD requests).  There is no neat way of doing this
     % so we will back everything off.
 
--record(state, {bookie :: pid(),
+-record(state, {bookie :: pid() | undefined,
                 reference :: reference(),
                 partition :: integer(),
                 db_path :: string(),
@@ -203,8 +203,10 @@ return_self(State) -> State#state.bookie.
 
 %% @doc Stop the leveled backend
 -spec stop(state()) -> ok.
-stop(#state{bookie=Bookie}) ->
-    ok = leveled_bookie:book_close(Bookie).
+stop(#state{bookie=Bookie}) when Bookie =/= undefined ->
+    ok = leveled_bookie:book_close(Bookie);
+stop(_State) ->
+    ok.
 
 
 %% @doc Retrieve an object from the leveled backend as a binary
@@ -582,7 +584,7 @@ fold_heads(FoldHeadsFun, Acc, Opts, #state{bookie=Bookie}) ->
 -spec drop(state()) -> {ok, state()} | {error, term(), state()}.
 drop(#state{bookie=Bookie}=State) ->
     ok = leveled_bookie:book_destroy(Bookie),
-    {ok, State}.
+    {ok, State#state{bookie = undefined}}.
 
 %% @doc Returns true if this leveled backend contains any
 %% non-tombstone values; otherwise returns false.
